fe84332d3bbf66e7d36d5f63b08fc56fb56b48fb
Serhii Bahlai <sbahl@softserveinc.com>
2019-03-06 16:25:43 +0200

    9 task done

--- 9.py
+++ 9.py
@@ -1,9 +1,22 @@
 def brack_eval(str):
-    for in, value in enumerate(str):
+    int = 0
+    for i, value in enumerate(str):
+        if value == "[":
+            int += 1
+        elif value == "]":
+            if int > 0:
+                int -= 1
+            else:
+                continue
+        else:
+            return print("String incorrect")
+    if int == 0:
+        print("Brackets are fine")
+    else:
+        print("NOT OK")
 
 
 
 
-
-
-x = "[]["
+x = "[]p"
+brack_eval(x)
